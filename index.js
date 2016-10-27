64fffae192a7bd6b324004423e972f7d703702ee
luwen-huang <huang.luwen@gmail.com>
2016-10-27 10:30:47 -0600

    exposing dao

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 /* requires and exposes the separate components of the app */
 
-var dao = window.dao || {};
-
-//dao = require('./src/dao.js');
-dao = require('./dist/dao.js');
-
-// make dao available globally
-window.dao = dao;
-global.dao = dao;
-
-module.exports = dao;
+module.exports = require('./dist/dao.js');
